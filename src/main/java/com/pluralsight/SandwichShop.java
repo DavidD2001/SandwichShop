bf4564273aa4d11ae2ccff1761587504e3ab2ab9
David D. <daviddorsey679@yahoo.com>
2025-04-17 20:30:15 -0700

    Initial commit - SandWichShop Exercise

---
+++ src/main/java/com/pluralsight/SandwichShop.java
@@ -0,0 +1,46 @@
+package com.pluralsight;
+import java.util.Scanner;
+public class SandwichShop {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter the size of Sandwich(Regular=1, Large=2): ");
+        int size = scanner.nextInt();
+
+        double basePrice;
+        if (size == 1) {
+            basePrice = 5.45;
+        } else if (size == 2) {
+            basePrice = 8.95;
+        } else {
+            System.out.println("Sorry the requested size is invalid, please try again.");
+            return;
+        }
+
+        System.out.println("Please enter your age: ");
+        int age = scanner.nextInt();
+
+        double agediscountRate = 0.00;
+        if (age <= 17) {
+            agediscountRate = 0.10;
+        } else if (age >= 65) {
+            agediscountRate = 0.20;
+        }
+        double ageDiscount = basePrice * agediscountRate;
+        double totalPrice = basePrice - ageDiscount;
+
+        if (age <= 17) {
+            System.out.println("Your total is: " + totalPrice);
+        } else if (age >= 65) {
+            System.out.println("Your total is: " + totalPrice);
+        } else {
+            System.out.println("Your Total Price is: " + basePrice);
+
+
+
+    }scanner.close();
+
+
+}
+
+}
